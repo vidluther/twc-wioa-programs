a856c902b9b70d574ea7b863ee58b66895c6e3bb
Vid Luther <vid@luther.io>
2021-10-25 21:28:39 -0500

    removed the default welcome page and started using a layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>{{ $title ?? 'TWC-WIOA Program List' }}</title>
+    </head>
+    <body>
+        <h1>TWC-WIOA Program List </h1>
+        <hr/>
+        {{ $slot }}
+    </body>
+</html>
