2252b232a85bb610fbe09e2fc0eb6d0d3c0a4139
Vid Luther <vid@luther.io>
2021-10-25 23:11:20 -0500

    actually using tailwindcss in our layout and body
    Still looks fugly.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
-   Welcome to the TWC-WIOA Program List
+    <p>
+        This is the list of vendors and programs that are available for the TWC
+        <blockquote> The Workforce Innovation and Opportunity Act (WIOA) is designed to help job seekers secure the employment, education, training and support services necessary to succeed in the labor market and to match employers with the skilled workers they need to compete in the global economy.
+
+        Every year the core programs that form the pillars of WIOA help tens of millions of job seekers and workers connect to good jobs and acquire the skills and credentials needed to obtain them. With the implementation of WIOA, the Texas workforce system will continue to be a market-driven system, responding to the needs of employers and preparing workers for jobs that are available now and in the future.
+    </blockquote>
+
+    <br /> Basically, if you're trying to learn a new skill, and want the government to pay for it, in Texas, you need to
+    make sure you go to one of these guys, once TWC determines you qualify for assistance, they'll write these guys a check.
+    </p>
+
 </x-layout>
