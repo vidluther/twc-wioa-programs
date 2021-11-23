de34ade811befd7da4e859516a0bb0495f000ef7
Vid Luther <vid@luther.io>
2021-11-22 21:30:17 -0600

    Down for maintenance page
    view that we render when we run php artisan down .. this way we can deploy in peace.

---
+++ resources/views/dfm.blade.php
@@ -0,0 +1,9 @@
+<html> 
+ <head> <title> DFM or Deploy</title> 
+ </head> 
+ <body> 
+ 
+<h2> The site is down for maintenance. Or a new version is being deployed. Try again in a few minutes.  </h2> 
+
+</body> 
+</html> 
