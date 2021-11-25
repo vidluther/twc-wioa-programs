c433bc4469809a9739eb07bb869fbd1f67ad4117
Vid Luther <vid@luther.io>
2021-11-24 21:52:51 -0600

    loading records from MongoDB now.

--- resources/views/programs/all.blade.php
+++ resources/views/programs/all.blade.php
@@ -9,32 +9,30 @@
                 <tr
                     class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
                 >
-                    <th class="px-4 py-3">Name</th>
-                    <th class="px-3 py-3">Description </th>
-                    <th class="px-4 py-3">Outcome</th>
-                    <th class="px-2 py-3">Cost</th>
-                    <th class="px-1 py-3">Hours</th>
-                    <th class="px-1 py-3">Weeks</th>
+                    <th class="px-1 py-1">Name</th>
+                    <th class="px-1 py-1">Description </th>
+                    <th class="px-1 py-1">Outcome</th>
+                    <th class="px-1 py-1">Cost</th>
+                    <th class="px-1 py-1">Hours</th>
+                    <th class="px-1 py-1">Weeks</th>
                 </tr>
                 </thead>
 
                 <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                 @foreach ($programs AS $program)
-
-
                 <tr class="text-gray-700 dark:text-gray-400">
-                    <td class="px-4 py-3">
+                    <td class="px-1 py-1">
                         <a href="/programs/{{ $program->twc_id }}" class="font-semibold">
-                            {!! $program->name !!}
+                            {!! $program->program_name !!}
                         </a>
                     </td>
-                    <td class="px-3 py-3">{{ $program->description }} </td>
+                    <td class="px-1 py-1">{{ $program->program_description }} </td>
 
 
-                    <td> {!! $program->outcome !!} </td>
-                    <td class="px-2 py-3 text-xs"> ${{ $program->req_cost }}</a></td>
-                    <td class="px-2 py-3 text-xs"> {{ $program->length_hours }}</a></td>
-                    <td class="px-2 py-3 text-xs"> {{ $program->length_weeks }}</a></td>
+                    <td> {!! $program->program_outcome !!} </td>
+                    <td class="px-2 py-3 text-xs"> ${{ $program->program_cost_tuition_and_fees }}</a></td>
+                    <td class="px-2 py-3 text-xs"> {{ $program->program_length_hours }}</a></td>
+                    <td class="px-2 py-3 text-xs"> {{ $program->program_length_weeks }}</a></td>
                 </tr>
                 @endforeach
                 </tbody>
