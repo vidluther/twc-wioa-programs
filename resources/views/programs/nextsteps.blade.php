15ff293f09301da4a3c86f8e4ab18412e09777f8
Vid Luther <vid@luther.io>
2021-12-19 00:41:43 -0600

    hopefully a better about page and next steps in general.

--- resources/views/programs/nextsteps.blade.php
+++ resources/views/programs/nextsteps.blade.php
@@ -1,6 +1,11 @@
-<h3 class="my-6 text-2xl font-semibold text-gray-700"> How to Apply </h3>
-1. Apply for the program at <a class="underline" href="{{ $program->program_url }}"> {{ $program->provider_name }}</a> website. <br />
-2. Visit the <a class="underline" href="{{ $local_twc_website }}" target="new"> your local Workforce Solutions office </a> and tell them you have
-enrolled in the program, and need to see if you're eligible for financial assistance.  <br />
+<span class="prose">
+<h2 class="py-6 font-semibold text-gray-700"> How to Apply </h2>
+    <ol>
+    <li> Apply for the program at <a class="underline" href="{{ $program->program_url }}"> {{ $program->provider_name }}</a> website. <br /></li>
+    <li> Visit the <a class="underline" href="{{ $local_twc_website }}" target="new"> your local Workforce Solutions office </a> and tell them you have
+enrolled in the program, and need to see if you're eligible for financial assistance.  </li>
+    </ol>
+
+    </span>
 <span clas="px-4 py-4"> </span>
 
