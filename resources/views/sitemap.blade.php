8dc8de4a1db2e31f67ad671dac5a3f90627ebbc8
Vid Luther <vid@luther.io>
2021-12-29 17:26:39 -0600

    sitemap includes privacy policy now and changed frequency

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,9 +1,15 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{url('about')}}</loc>
         <lastmod>2021-12-19T05:45:04Z</lastmod>
-        <changefreq>daily</changefreq>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{url('privacy-policy')}}</loc>
+        <lastmod>2021-12-19T05:45:04Z</lastmod>
+        <changefreq>yearly</changefreq>
         <priority>0.8</priority>
     </url>
     @foreach ($programs as $post)
