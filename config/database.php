33efd357690f24a93e19c71cd900bec069849327
Shift <shift@laravelshift.com>
2024-09-29 21:31:45 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files easier, Shift defaulted them and merged your true customizations - where ENV variables may not be used.

--- config/database.php
+++ config/database.php
@@ -15,10 +15,8 @@ return [
     |
     */
 
-    //    'default' => env('DB_CONNECTION', 'mysql'),
-    // Changed to use MongoDB as per https://www.mongodb.com/compatibility/mongodb-laravel-intergration
+    'default' => env('DB_CONNECTION', 'mysql'),
 
-    'default' => env('DB_CONNECTION', 'mongodb'),
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -41,6 +39,7 @@ return [
             'dsn' => env('MONGO_DB_URI', 'mongodb+srv://username:password@<atlas-cluster-uri>/myappdb?retryWrites=true&w=majority'),
             'database' => env('MONGO_DB_NAME'),
         ],
+
 
         'sqlite' => [
             'driver' => 'sqlite',
@@ -96,6 +95,8 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
+            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
     ],
@@ -136,7 +137,8 @@ return [
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
@@ -144,7 +146,8 @@ return [
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
