0f8d0a5127adde2f606492fecdbd7c2cd2e1c119
Shift <shift@laravelshift.com>
2024-09-29 21:34:21 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -11,20 +11,5 @@ return [
 
     'google_analytics_id' => env('GOOGLE_ANALYTICS_ID', 'G-XXXXXX'),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\MetaTagsServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
 ];
